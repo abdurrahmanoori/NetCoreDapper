1208a54ee4c3d26a784be68e70f618e8c5acb455
Abdurrahman <Abdurrahmannoori10@gmail.com>
2023-12-09 11:30:41 +0430

    intal commmit

---
+++ NetCoreDapper.Application/Interfaces/IProductInterface.cs
@@ -0,0 +1,13 @@
+﻿using NetCoreDapper.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreDapper.Application.Interfaces
+{
+    public interface IProductInterface : IGenericRepository<Product>
+    {
+    }
+}
